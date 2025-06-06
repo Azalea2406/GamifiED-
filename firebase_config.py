aaf3116cf81687b3819622a0b6ce601e894a1f7e
Ishrath Tabassum <148183636+Azalea2406@users.noreply.github.com>
2025-06-06 08:48:15 +0530

    Create firebase_config.py

---
+++ firebase_config.py
@@ -0,0 +1,17 @@
+import pyrebase
+
+firebase_config = {
+    "apiKey": "AIzaSyBAxO8ACbviAbFZEopMiW-MD5WZX17TE4c",
+    "authDomain": "gamified-2064.firebaseapp.com",
+    "databaseURL": "https://gamified-2064-default-rtdb.firebaseio.com", 
+    "projectId": "gamified-2064",
+    "storageBucket": "gamified-2064.firebasestorage.app",
+    "messagingSenderId": "408735255781",
+    "appId": "1:408735255781:web:fc9e41339a6feea8e1180c",
+    "measurementId": "G-1JLQ89LZPC"
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
+db = firebase.database()
+
